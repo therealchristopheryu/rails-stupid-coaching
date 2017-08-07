89737485ce43c9af35e3bd32c4c4a89e59f23f23
Christopher Yu <cfyu@eng.ucsd.edu>
2017-08-07 14:12:36 +0800

    Rails: Routes and Get completed - need CSS

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,28 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:answer]
+    @answer = coach_answer_enhanced(@question)
+  end
+end
+
+
+def coach_answer(question)
+  if question.include? "?"
+    return "Silly question, get dressed and go to work!"
+  elsif question == "I am going to work right now!"
+    ""
+  else
+    return "I don't care, get dressed and go to work!"
+  end
+end
+
+def coach_answer_enhanced(question)
+  if question.upcase.include? "?"
+    return "I can feel your motivation! Silly question, get dressed and go to work!"
+  elsif question == "I am going to work right now!".upcase
+    ""
+  else
+    return "I can feel your motivation! I don't care, get dressed and go to work!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'questions/ask'
+
+  get 'questions/answer'
+
   get 'ask', to: 'questions#ask'
 
   get 'answer', to: 'questions#answer'
